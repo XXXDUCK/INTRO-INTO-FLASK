36dcbfa3564618e774beb8fa4c8ea9603801f052
XXXDUCK <niksalik2002@gmail.com>
2020-01-24 11:10:16 +0000

    gunicorn==19.9.0

--- main.py
+++ main.py
@@ -2,22 +2,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-  return  "<a href='/home'>Hi!</a>"
+    return "<a href='/home'>Hi!</a>"
+
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 
 @app.route('/about')
 def about():
-  return render_template('about.html')
+    return render_template('about.html')
+
 
 @app.route('/contact')
 def contact():
-  return render_template ('contact.html')
+    return render_template('contact.html', phone=7442457)
 
-@app.route('/home')
-def home():
-  return render_template('home.html')    
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port = 5222, threaded = True, debug = True)
+    app.run(host='0.0.0.0', port=5222, threaded=True, debug=True)
